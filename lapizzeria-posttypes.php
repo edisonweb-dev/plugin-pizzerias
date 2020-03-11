bdcfa70487e6e4efb3d4934779e1f15858368ff5
Edison Perdomo <edisonperdomodev@gmail.com>
2020-03-11 17:41:15 -0400

    Inicio del plugin

---
+++ lapizzeria-posttypes.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin Name: La Pizzeria - Especialidades
+    Plugin URI: 
+    Description: Añade Post Types al Sitio Web
+    Version: 1.0.0
+    Author: Edison Perdomo
+    Author URI: 
+    Text Domain: 
+*/
